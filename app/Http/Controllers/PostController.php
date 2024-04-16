d66d1ce57d2d027382aa079af480c0bbb9447011
KyleKibe <kylekibe26@gmail.com>
2024-04-16 16:29:17 +0900

    完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,14 @@ class PostController extends Controller
     {
         return view('posts.show')->with(['post' => $post]);
     }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
+  public function store(Request $request, Post $post)
+  {
+      dd($request->all());
+  }
 }
-?>
-
